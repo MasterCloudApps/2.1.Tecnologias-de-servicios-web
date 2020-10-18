c51a55a4b32057684597b806e6fef9e8ff4703f0
micaelgallego <micael.gallego@gmail.com>
2020-10-18 18:54:02 +0200

    Split ampg-ejem between producer and consumer

--- tema5/Java/amqp-ejem1-consumer/src/main/java/es/codeurjc/mastercloudapps/amqp/Application.java
+++ tema5/Java/amqp-ejem1-consumer/src/main/java/es/codeurjc/mastercloudapps/amqp/Application.java
@@ -1,14 +1,19 @@
 package es.codeurjc.mastercloudapps.amqp;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public Queue myQueue() {
+    	return new Queue("messages", false);
 	}
 }
---
+++ tema5/Java/amqp-ejem1-producer/src/main/java/es/codeurjc/mastercloudapps/amqp/Application.java
@@ -0,0 +1,21 @@
+package es.codeurjc.mastercloudapps.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public Queue myQueue() {
+    	return new Queue("messages", false);
+	}
+}
