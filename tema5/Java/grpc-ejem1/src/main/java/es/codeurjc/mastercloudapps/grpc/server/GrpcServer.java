adac28ae8c0ffd305c06d9c32d25f60ad8ae23b5
micaelgallego <micael.gallego@gmail.com>
2020-10-18 18:55:00 +0200

    Update grpc example

--- tema5/Java/grpc-ejem1/src/main/java/es/codeurjc/mastercloudapps/grpc/server/GrpcServer.java
+++ tema5/Java/grpc-ejem1/src/main/java/es/codeurjc/mastercloudapps/grpc/server/GrpcServer.java
@@ -6,13 +6,16 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class GrpcServer {
+
     public static void main(String[] args) throws IOException, InterruptedException {
-    	
+      
+        System.out.println("Starting server...");
+
         Server server = ServerBuilder.forPort(9090)
           .addService(new HelloService()).build();
-
-        System.out.println("Starting server...");
+       
         server.start();
+        
         System.out.println("Server started!");
         
         server.awaitTermination();
