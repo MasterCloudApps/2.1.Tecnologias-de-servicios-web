249352cde11b9ce17594932f3960a788cb953018
micaelgallego <micael.gallego@gmail.com>
2020-11-29 20:28:06 +0100

    Update form example

--- tema2-web/Java/web_ejem5/src/main/java/es/codeurjc/web/GreetingController.java
+++ tema2-web/Java/web_ejem5/src/main/java/es/codeurjc/web/GreetingController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String greeting(Model model, @RequestParam String name) {
+	public String greeting(Model model, @RequestParam String userName) {
 
-		model.addAttribute("name", name);
+		model.addAttribute("name", userName);
 
 		return "greeting_template";
 	}
