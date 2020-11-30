62eb016047751027b3bfef56685df68869e568aa
micaelgallego <micael.gallego@gmail.com>
2020-11-30 01:32:43 +0100

    Refactor exercise 6 based on new exercise 5

--- tema2-web/Java/web_ejer6/src/main/java/es/codeurjc/board/Application.java
+++ tema2-web/Java/web_ejer6/src/main/java/es/codeurjc/board/Application.java
@@ -1,4 +1,4 @@
-package es.codeurjc.tablonanuncios;
+package es.codeurjc.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ tema2-web/Java/web_ejer6/src/main/java/es/codeurjc/board/controller/PostController.java
@@ -0,0 +1,72 @@
+package es.codeurjc.board.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import es.codeurjc.board.model.Post;
+import es.codeurjc.board.service.PostService;
+import es.codeurjc.board.service.UserSession;
+
+@Controller
+public class PostController {
+
+	@Autowired
+	private PostService postService;
+	
+	@Autowired
+	private UserSession userSession;
+
+	@GetMapping("/")
+	public String showPosts(Model model, HttpSession session) {
+
+		model.addAttribute("posts", postService.findAll());
+		model.addAttribute("welcome", session.isNew());
+
+		return "index";
+	}
+
+	@GetMapping("/post/new")
+	public String newPostForm(Model model) {
+
+		model.addAttribute("user", userSession.getUser());
+
+		return "new_post";
+	}
+	
+	@PostMapping("/post/new")
+	public String newPost(Model model, Post post) {
+
+		postService.save(post);
+		
+		userSession.setUser(post.getUser());
+		userSession.incNumPosts();
+		
+		model.addAttribute("numPosts", userSession.getNumPosts());
+
+		return "saved_post";
+	}
+
+	@GetMapping("/post/{id}")
+	public String showPost(Model model, @PathVariable long id) {
+
+		Post post = postService.findById(id);
+
+		model.addAttribute("post", post);
+
+		return "show_post";
+	}
+	
+	@GetMapping("/post/{id}/delete")
+	public String deletePost(Model model, @PathVariable long id) {
+
+		postService.delete(id);
+
+		return "deleted_post";
+	}
+}
