46c2a2f517105cb929125d03346b5953ab2ea0be
NicolasRodriguezUribe <nicolas.rodriguez@urjc.es>
2021-01-11 23:08:23 +0100

    Prueba subida Nico

---
+++ tema8-seguridad/security_ejem1/src/main/java/es/urjc/code/security/WebController.java
@@ -0,0 +1,14 @@
+package es.urjc.code.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+}
