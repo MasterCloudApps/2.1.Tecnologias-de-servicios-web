2141c7ae999bb1990d38eaa8df4f5a7605a0824c
Micael Gallego <micael.gallego@gmail.com>
2021-11-23 23:18:07 +0100

    Update web-ejem1 to ESM

--- tema2-web/Node/web-ejem1/app.js
+++ tema2-web/Node/web-ejem1/app.js
@@ -1,11 +1,9 @@
-let express = require('express');
+import express from 'express';
 
-let app = express();
+const app = express();
 
 app.get('/', (req, res) => {
    res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-   console.log('Example app listening on port 3000!');
-});
+app.listen(3000, () => console.log('Listening on port 3000!'));
