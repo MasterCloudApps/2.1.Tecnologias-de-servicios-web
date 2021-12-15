628c1e9e08f9d3d3645a4b1628badb6da9fe1334
Patxi Gortázar <patxi.gortazar@gmail.com>
2021-12-15 11:24:42 +0100

    Add rabbitmq-stream-ejem2 with native client

---
+++ tema7-comunicacion/Java/comunicacion-rabbitmq-stream-ejem2-producer/src/main/java/es/codeurjc/mastercloudapps/amqp/Publisher.java
@@ -0,0 +1,56 @@
+package es.codeurjc.mastercloudapps.amqp;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.rabbitmq.stream.Environment;
+import com.rabbitmq.stream.Message;
+import com.rabbitmq.stream.Producer;
+
+@Component
+public class Publisher {
+
+	private int numData;
+	private Environment environment;
+	private String stream = "stream-data";
+	private Producer producer;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("Connecting...");
+		
+		environment = Environment.builder()
+				.build();  
+		
+		environment.streamCreator().stream(stream).create();
+		
+		producer = environment.producerBuilder()  
+		        .stream(stream)
+		        .build();
+	}
+	
+	@Scheduled(fixedRate = 1000)
+	public void sendMessage() {
+		
+		String data = "Data " + numData++;
+		
+		System.out.println("publishToQueue: '" + data + "'");
+				
+		Message message = producer.messageBuilder()
+			.addData(data.getBytes())
+			.build();
+		
+		producer.send(message, confirmationStatus -> System.out.println("Message sent!"));
+
+	}
+	
+	@PreDestroy
+	public void close() {
+		producer.close();
+		
+		environment.close();
+	}
+}
