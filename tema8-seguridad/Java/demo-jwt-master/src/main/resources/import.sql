aba23ce7c2e513e01a3b1800afdcdc1be62c2ba4
NicolasRodriguezUribe <38919406+NicolasRodriguezUribe@users.noreply.github.com>
2022-01-09 22:26:03 +0100

    Deleted old files
    The files of the last year have been deleted

--- tema8-seguridad/Java/demo-jwt-master/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-
--- Inicializamos la BD
-
-INSERT INTO Usuario (username, password) VALUES ('admin', '$2a$10$XURPShQNCsLjp1ESc2laoObo9QZDhxz73hJPaEv7/cBha4pk0AgP.');
--- tema8-seguridad/Node/nodejs-restful-api-master2/nodejs-restful-api-master/db.js
+++
@@ -1,2 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
