8835dd0f86cf6d8842b57c7a5ea6c405ac7db11a
Micael Gallego <micael.gallego@gmail.com>
2022-02-17 18:32:06 +0100

    Add PostgreSQL example (ejem24)

---
+++ tema4-bd/Java/bd_ejem24/src/main/java/es/codeurjc/db/PostController.java
@@ -0,0 +1,36 @@
+package es.codeurjc.db;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+	@Autowired
+	private PostRepository posts;
+
+	@PostConstruct
+	public void init() {
+		posts.save(new Post("Pepe", "Hola caracola", "XXXX"));
+		posts.save(new Post("Juan", "Hola caracola", "XXXX"));
+	}
+
+	@GetMapping("/")
+	public Page<Post> getPosts(@RequestParam(required = false) String user, Pageable page) {
+
+		if (user != null) {
+			return posts.findByUser(user, page);
+		} else {
+			return posts.findAll(page);
+		}
+	}
+
+}
