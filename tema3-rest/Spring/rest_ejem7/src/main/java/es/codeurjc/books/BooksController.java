099c8e9b765ef45cda28bdd45a637fbc7d18fe0d
Micael Gallego <micael.gallego@gmail.com>
2022-11-15 19:28:56 +0100

    Change classes by records in rest_ejem7 (RestTemplate with classes)

--- tema3-rest/Spring/rest_ejem7/src/main/java/es/codeurjc/books/BooksController.java
+++ tema3-rest/Spring/rest_ejem7/src/main/java/es/codeurjc/books/BooksController.java
@@ -1,43 +1,42 @@
 package es.codeurjc.books;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-class BooksResponse {
-	public List<Book> items;
-}
+import java.util.ArrayList;
+import java.util.List;
 
-class Book {
-	public VolumeInfo volumeInfo;
-}
 
-class VolumeInfo {
-	public String title;
-}
 
 @RestController
 public class BooksController {
 
-	@GetMapping("/booktitles")
-	public List<String> getBookTitles(@RequestParam String title) {
-
-		RestTemplate restTemplate = new RestTemplate();
-
-		String url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title;
-
-		BooksResponse data = restTemplate.getForObject(url, BooksResponse.class);
-
-		List<String> bookTitles = new ArrayList<String>();
-
-		for (Book book : data.items) {
-			bookTitles.add(book.volumeInfo.title);
-		}
-
-		return bookTitles;
+	record BooksResponse(List<Book> items) {
 	}
+
+	record Book(VolumeInfo volumeInfo) {
+	}
+
+	record VolumeInfo(String title) {
+	}
+
+    @GetMapping("/booktitles")
+    public List<String> getBookTitles(@RequestParam String title) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title;
+
+        BooksResponse data = restTemplate.getForObject(url, BooksResponse.class);
+
+        List<String> bookTitles = new ArrayList<>();
+
+        for (Book book : data.items()) {
+            bookTitles.add(book.volumeInfo().title());
+        }
+
+        return bookTitles;
+    }
 }
