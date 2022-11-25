86f0e45139346489cdc86028c62fab75ec9c1705
Micael Gallego <micael.gallego@gmail.com>
2022-11-25 20:39:48 +0100

    Restore Mundo in web_ejem1

--- tema2-web/Spring/web_ejem1/src/main/java/es/codeurjc/web/GreetingController.java
+++ tema2-web/Spring/web_ejem1/src/main/java/es/codeurjc/web/GreetingController.java
@@ -10,7 +10,7 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public String greeting(Model model) {
 
-		model.addAttribute("name", "Mundo2");
+		model.addAttribute("name", "Mundo");
 
 		return "greeting_template";
 	}
