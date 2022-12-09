4b5b1beb13ce9f243c2b64d2efe49c15c5ff6b30
Micael Gallego <micael.gallego@gmail.com>
2022-12-09 11:08:27 +0100

    Remove not used tests

--- tema6-reactive/Quarkus/reactive-ejem1-books-reacive-quarkus/src/test/java/es/codeurjc/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package es.codeurjc;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
